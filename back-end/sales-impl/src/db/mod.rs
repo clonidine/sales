165dd8f08aaf222b0e3c393ee330d7123efd3f40
miggx <lenad3vil@protonmail.com>
2023-07-30 15:38:59 -0300

    (Sales-Impl) Renaming

--- back-end/sales-impl/src/db/mod.rs
+++ back-end/sales-impl/src/db/mod.rs
@@ -1,11 +1,11 @@
-pub mod implementations;
+pub mod db_impl;
 pub mod mysql;
 
 #[cfg(test)]
 mod db_tests {
     use sales_api::api::provider::Database;
 
-    use crate::db::implementations::DBImplementation;
+    use crate::db::db_impl::DBImplementation;
 
     #[test]
     fn opening_conn() {
