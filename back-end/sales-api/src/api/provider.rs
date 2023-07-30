2448a15c1e89b41a166aa3419b5483183a3d2809
miggx <lenad3vil@protonmail.com>
2023-07-30 15:17:57 -0300

    (Sales-API) Added self parameter in 'provider'

--- back-end/sales-api/src/api/provider.rs
+++ back-end/sales-api/src/api/provider.rs
@@ -1,6 +1,6 @@
 use mysql::PooledConn;
 
 pub trait Database {
-    fn connect() -> Result<PooledConn, String>;
+    fn connect(&self) -> Result<PooledConn, String>;
     fn get_db_url() -> Result<String, String>;
 }
