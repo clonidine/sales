bdc308713c5ad6fd31ad518b69e639ed7fd1a21a
miggx <lenad3vil@protonmail.com>
2023-07-30 13:18:52 -0300

    (Sales-API) Created methods and functions for 'Repository' trait

--- back-end/sales-api/src/api/repository.rs
+++ back-end/sales-api/src/api/repository.rs
@@ -1,1 +1,7 @@
-pub trait Repository {}
+pub trait Repository {
+    fn save(&self) -> Result<(), String>;
+    fn delete(&self) -> Result<(), String>;
+    fn find_one(&self, id: usize) -> Option<&Self>;
+    fn create_table(&self) -> Result<(), String>;
+    fn get_table_name() -> &'static str;
+}
