418038d9d512a8744bc9b3e72aab6d5d4133f3e4
miggx <lenad3vil@protonmail.com>
2023-08-01 01:52:38 -0300

    Added 'sqlx' crate

--- back-end/sales-api/src/api/provider.rs
+++ back-end/sales-api/src/api/provider.rs
@@ -1,6 +1,6 @@
-use mysql::PooledConn;
+use sqlx::{AnyConnection, Error};
 
 pub trait Database {
-    fn connect(&self) -> Result<PooledConn, String>;
+    fn connect(&self) -> Result<AnyConnection, Error>;
     fn get_db_url() -> Result<String, String>;
 }
