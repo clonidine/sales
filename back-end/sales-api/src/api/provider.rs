40bfa1f126b3c4aa70b2a0fae45ff1c854f5c5b8
miggx <lenad3vil@protonmail.com>
2023-08-01 21:59:13 -0300

    (Sales-API) Removing 'self' parameter

--- back-end/sales-api/src/api/provider.rs
+++ back-end/sales-api/src/api/provider.rs
@@ -1,6 +1,6 @@
 use sqlx::{AnyConnection, Error};
 
 pub trait Database {
-    fn connect(&self) -> Result<AnyConnection, Error>;
+    fn connect() -> Result<AnyConnection, Error>;
     fn get_db_url() -> Result<String, String>;
 }
