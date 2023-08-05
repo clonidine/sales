e3e9ab3074cfb6ae59449bbe5078313e323f737f
miggx <lenad3vil@protonmail.com>
2023-08-05 11:08:27 -0300

    (Sales-API) Object-safe Database trait

--- back-end/sales-api/src/api/provider.rs
+++ back-end/sales-api/src/api/provider.rs
@@ -3,6 +3,6 @@ use sqlx::{AnyConnection, Result};
 
 #[async_trait]
 pub trait Database {
-    async fn connect() -> Result<AnyConnection>;
-    async fn get_db_url() -> Result<String>;
+    async fn connect(&self) -> Result<AnyConnection>;
+    async fn get_db_url(&self) -> Result<String>;
 }
