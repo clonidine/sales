18a62d874a78c1eca2e6f8d827bc773b0e6799cb
miggx <lenad3vil@protonmail.com>
2023-08-05 16:37:27 -0300

    (Sales-Impl) Specifying MySQL and changing 'get db url' return

--- back-end/sales-impl/src/db/db.rs
+++ back-end/sales-impl/src/db/db.rs
@@ -3,29 +3,21 @@ use std::env;
 use async_trait::async_trait;
 use dotenv::dotenv;
 use sales_api::api::provider::Database;
-use sqlx::{any::install_default_drivers, AnyConnection, Connection, Error};
+use sqlx::{any::install_default_drivers, Connection, MySqlConnection, Result};
 
 pub struct DatabaseImpl;
 
 #[async_trait]
 impl Database for DatabaseImpl {
-    async fn connect() -> Result<AnyConnection, Error> {
+    async fn connect() -> Result<MySqlConnection> {
         install_default_drivers();
 
-        let db_url = Self::get_db_url().await;
+        let db_url = Self::get_db_url().await?;
 
-        match db_url {
-            Ok(db_url) => {
-                let connection = AnyConnection::connect(&db_url).await?;
-
-                Ok(connection)
-            }
-
-            Err(e) => panic!("{}", e),
-        }
+        Ok(MySqlConnection::connect(&db_url).await?)
     }
 
-    async fn get_db_url() -> Result<String, String> {
+    async fn get_db_url() -> Result<String> {
         let loaded_var_environment = dotenv().ok();
 
         match loaded_var_environment {
@@ -55,6 +47,7 @@ mod db_tests {
 
         match connection {
             Ok(_) => assert!(true),
+
             Err(e) => {
                 println!("Error while getting connection: {}", e);
                 assert!(false)
