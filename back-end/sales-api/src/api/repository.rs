5eb1b183b0e67b8ba217c3ff19337c7062c884ec
miggx <lenad3vil@protonmail.com>
2023-08-05 12:15:42 -0300

    (Sales-API) Changing function return

--- back-end/sales-api/src/api/repository.rs
+++ back-end/sales-api/src/api/repository.rs
@@ -1,9 +1,9 @@
 use async_trait::async_trait;
-use sqlx::Result;
+use sqlx::{Result, mysql::MySqlRow};
 
 #[async_trait]
 pub trait Repository<T> {
-    async fn save(obj: &T) -> Result<()>;
+    async fn save(obj: &T) -> Result<MySqlRow>;
     async fn delete(id: usize) -> Result<()>;
     async fn find_one(id: usize) -> Result<T>;
     async fn create_table() -> Result<()>;
