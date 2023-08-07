49c7e964deb93da159437a0a73480642d50b9b0b
miggx <lenad3vil@protonmail.com>
2023-08-06 23:34:46 -0300

    (Sales-API) Updating 'Cart' model from 'UUID' to 'u64'

--- sales-api/src/api/domain/cart.rs
+++ sales-api/src/api/domain/cart.rs
@@ -1,11 +1,9 @@
 use std::collections::HashMap;
 
-use sqlx::types::Uuid;
-
 use super::product::Product;
 
 #[derive(Debug)]
 pub struct Cart {
     pub id: i64,
-    pub products: HashMap<Uuid, Product>,
+    pub products: HashMap<u64, Product>,
 }
